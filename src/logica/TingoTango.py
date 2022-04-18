a2000a5687c1f664fc8063935e518d03e986b4b6
WalterPimentel <70056787@continental.edu.pe>
2022-04-18 16:49:53 -0500

    TingoTnago refactorizacion v2.2

--- src/logica/TingoTango.py
+++ src/logica/TingoTango.py
@@ -1,9 +1,11 @@
 class TingoTango:
     def textoTingoTango(self, numero):
-        if (numero % 5 == 0) and (numero % 3 == 0):
-            return "TingoTango"
-        elif(numero % 3 == 0):
-            return "Tingo"
-        elif(numero % 5 == 0):
-            return "Tango"
-        return str(numero)
+        s = ""
+        if(numero % 3 == 0):
+            s += "Tingo"
+        if(numero % 5 == 0):
+            s += "Tango"
+        if(len(s)):
+            return s
+        else:
+            return str (numero)
